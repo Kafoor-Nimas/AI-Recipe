53c81f19a0a22ecbddf666a6749ce1f4daf90d10
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-09 10:31:45 +0530

    started buliding landing page

---
+++ frontend/app/page.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const { has } = await auth();
+  const subscriptionTier = has({ plan: "pro" }) ? "pro" : "free";
+
+  return (
+    <div className="min-h-screen bg-stone-50 text-stone-900">
+      <section className="pt-32 pb-20 px-4">
+        <Button variant="primary" size="xl">
+          Subscribe
+        </Button>
+      </section>
+    </div>
+  );
+}
