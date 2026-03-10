7ec3344df13e91f04e5a2263833814a12b8f7aee
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-10 20:17:13 +0530

    refactor page component to display recipe of the day, categories, and areas

--- frontend/app/(main)/dashboard/page.jsx
+++ frontend/app/(main)/dashboard/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {
+  getAreas,
+  getCategories,
+  getRecipeOfTheDay,
+} from "@/actions/mealdb.actions";
+import React from "react";
 
-const page = () => {
+const page = async () => {
+  const recipeData = await getRecipeOfTheDay();
+  const categoriesData = await getCategories();
+  const areasData = await getAreas();
+
+  const recipeOfTheDay = recipeData?.recipe;
+  const categories = categoriesData?.categories || [];
+  const areas = areasData?.areas || [];
+
   return (
-    <div className='min-h-screen bg-stone-50 py-16 px-4'>DashboardPage</div>
-  )
-}
+    <div className="min-h-screen bg-stone-50 py-16 px-4">
+      <div className="max-w-6xl mx-auto">
+        <div className="mb-5">
+          <h1 className="text-5xl md:text-7xl font-bold text-stone-900 mb-4 tracking-tight leading-tight">
+            Fresh Recipes, Servd Daily 🔥
+          </h1>
+          <p className="text-xl text-stone-600 font-light max-w-2xl">
+            Discover thousands of recipes from around the world. Cook, create,
+            and savor.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
