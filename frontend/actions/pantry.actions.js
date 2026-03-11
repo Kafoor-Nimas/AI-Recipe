c7eb8d19a1a622d417fc3acc405377ad178eba9f
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-11 20:45:17 +0530

    feat: add Google Generative AI integration and implement scanPantryImage action

---
+++ frontend/actions/pantry.actions.js
@@ -0,0 +1,25 @@
+"use server"
+
+import { checkUser } from "@/lib/checkUser";
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const STRAPI_URL =
+  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
+const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY)
+
+export async function scanPantryImage(formData){
+    try {
+        const user = await checkUser();
+        if(!user){
+            throw new Error("User not authenticated")
+        }
+
+        const isPro = user.subscriptionTier === "pro"
+    } catch (error) {
+        
+    }
+}
