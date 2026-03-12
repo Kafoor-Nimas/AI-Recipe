bcfa260098b0fa69c8711c900faba0f3f3d429c1
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-12 10:54:32 +0530

    Apply Arcjet rate limit based on tier

--- frontend/actions/pantry.actions.js
+++ frontend/actions/pantry.actions.js
@@ -1,6 +1,8 @@
-"use server"
+"use server";
 
+import { freePantryScans, proTierLimit } from "@/lib/arcjet";
 import { checkUser } from "@/lib/checkUser";
+import { request } from "@arcjet/next";
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
@@ -9,20 +11,36 @@ const STRAPI_URL =
 const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-const genAI = new GoogleGenerativeAI(GEMINI_API_KEY)
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
-export async function scanPantryImage(formData){
-    try {
-        const user = await checkUser();
-        if(!user){
-            throw new Error("User not authenticated")
-        }
-
-        const isPro = user.subscriptionTier === "pro"
-
-        // Apply Arcjet rate limit based on tier
-        
-    } catch (error) {
-        
+export async function scanPantryImage(formData) {
+  try {
+    const user = await checkUser();
+    if (!user) {
+      throw new Error("User not authenticated");
     }
+
+    const isPro = user.subscriptionTier === "pro";
+
+    // Apply Arcjet rate limit based on tier
+    const arcjetClient = isPro ? proTierLimit : freePantryScans;
+
+    //create a request object for Arcjet
+    const req = await request();
+
+    const decision = await arcjetClient.protect(req, {
+      userId: user.clerkId,
+      requested: 1,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        throw new Error(
+          `Monthly scan limit reached. ${isPro ? "Please contact support if you need more scans" : "Upgrade to Pro for unlimited scans"}`,
+        );
+      }
+
+      throw new Error("Request denied by security system");
+    }
+  } catch (error) {}
 }
