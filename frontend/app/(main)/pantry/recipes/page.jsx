8fb0df570f9183d81446683eea9fc9a246ac559f
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-18 11:55:19 +0530

    start working UI for PantryRecipesPage

--- frontend/app/(main)/pantry/recipes/page.jsx
+++ frontend/app/(main)/pantry/recipes/page.jsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { getRecipesByPantryIngredients } from "@/actions/recipe.actions";
+import useFetch from "@/hooks/use-fetch";
 import React from "react";
 
 const PantryRecipesPage = () => {
+  const {
+    loading,
+    data: recipesData,
+    fn: fetchSuggestions,
+  } = useFetch(getRecipesByPantryIngredients);
+
   return <div>PantryRecipesPage</div>;
 };
 
