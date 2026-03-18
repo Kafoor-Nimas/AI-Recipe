18c87ecd05711d4653a253791044f8751d6ff5d6
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-18 10:54:38 +0530

    Apply Arcjet rate limit based on tier and create a request object for Arcjet

---
+++ frontend/actions/recipe.actions.js
@@ -0,0 +1,51 @@
+"use server";
+
+import { freePantryScans, proTierLimit } from "@/lib/arcjet";
+import { checkUser } from "@/lib/checkUser";
+import { request } from "@arcjet/next";
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const STRAPI_URL =
+  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
+const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+
+export async function getRecipesByPantryIngredients() {
+    try {
+        const user = await checkUser();
+        if(!user){
+            throw new Error("User not authenticated")
+        }
+
+        const isPro=user.subscriptionTier === "pro";
+
+        // Apply Arcjet rate limit based on tier
+            const arcjetClient = isPro ? proTierLimit : freePantryScans;
+        
+            //create a request object for Arcjet
+            const req = await request();
+        
+            const decision = await arcjetClient.protect(req, {
+              userId: user.clerkId,
+              requested: 1,
+            });
+        
+            if (decision.isDenied()) {
+              if (decision.reason.isRateLimit()) {
+                throw new Error(
+                  `Monthly scan limit reached. ${isPro ? "Please contact support if you need more scans" : "Upgrade to Pro for unlimited scans"}`,
+                );
+              }
+        
+              throw new Error("Request denied by security system");
+            }
+        
+    } catch (error) {
+        
+    }
+    
+}
